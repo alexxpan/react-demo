9598c70521862a594eb42419aaa52da70f6bb324
Alex Pan <alpan@Alexs-MacBook-Pro.local>
2017-03-15 19:39:37 -0700

    kind of fixed

--- src/Form.js
+++ src/Form.js
@@ -20,39 +20,54 @@ export default class Form extends Component {
 		this._handleSubmit = this._handleSubmit.bind(this);
 	}
 
-	_handleName(name) {
+	_handleName(name, type) {
 		var validate = true;
 		var character = name.charAt(0);
-		if (character != character.toUpperCase()) {
-			alert('Name not in correct format');
+		if (character !== character.toUpperCase()) {
+			validate = false;
 		}
 		var i = 1;
-		while (i <= name.length) {
+		while (i <= name.length && validate !== false) {
 			character = name.charAt(i);
-			if (character != character.toLowerCase()) {
+			if (character !== character.toLowerCase()) {
 				validate = false;
-				break;
 			}
-			i = i + 1;
+			i += 1;
 		}
-		if (validate == false) {
-			alert('Name not in correct format');
+		if (validate === false) {
+			if (type === "first") {
+				console.log("hello")
+				this.setState({
+			    	firstName: null,
+				});
+			
+			} else if (type === "last") {
+				this.setState({
+			    	lastName: null,
+				});
+			}
 		}
 	}
 
 	_handleEmail(email) {
 		var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     	var validate = re.test(email);
-		if (validate == false) {
+		if (validate === false) {
 			alert('Email not in correct format');
+			this.setState({
+		    	email: null,
+			});
 		} 
 	}
 
 	_handlePhone(phone) {
 		var re = /^(\()?\d{3}(\))?(-|\s)?\d{3}(-|\s)\d{4}$/;
 		var validate = re.test(phone);
-		if (validate == false) {
+		if (validate === false) {
 			alert('Phone not in correct format');
+			this.setState({
+		    	phone: null,
+			});
 		} 
 	}
 
@@ -62,8 +77,8 @@ export default class Form extends Component {
 		var email = this.state.email;
 		var phone = this.state.phone;
 
-		this._handleName(firstName);
-		this._handleName(lastName);
+		this._handleName(firstName, "first");
+		this._handleName(lastName, "last");
 		this._handleEmail(email);
 		this._handlePhone(phone);
 
@@ -79,7 +94,7 @@ export default class Form extends Component {
 
 	render() {
 		return (
-			<form onSubmit={this.handleSubmit}>
+			<form onSubmit={ this._handleSubmit }>
 				<Field header="First Name" name="firstName" handler={ this._handleChange }/>
 				<Field header="Last Name" name="lastName" handler={ this._handleChange }/>
 				<Field header="Email Address" name="email" handler={ this._handleChange }/>
@@ -95,7 +110,7 @@ class Field extends Component {
 		return (
 			<div className="field">
 				<div className="field__header">{ this.props.header }</div>
-				<input className="field__input" type="text" name = { this.props.name } onChange={ this.props.handler }/>
+				<input className="field__input" type="text" name={ this.props.name } onChange={ this.props.handler }/>
 			</div>
 		);
 	}
